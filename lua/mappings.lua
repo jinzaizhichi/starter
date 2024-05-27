e57bc8709eeb48969212ded81baf657ecdd3a9c2
jinzaizhichi <jinzaizhichi9888@gmail.com>
2024-05-27 16:36:05 +0800

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,13 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-
+map("i", "jj", "<ESC>")
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "x" }, "mm", "<Cmd>Translate<CR>", { desc = " Translate" })
+--     keys = {
+--       -- 可以换成其他你想映射的键
+--       { "mm", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = " Translate" },
+--       { "mk", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " Auto Play" },
+--       -- 目前这个功能的视窗还没有做好，可以在配置里将view.i改成hover
+--       { "mi", "<Cmd>TranslateInput<CR>", desc = " Translate From Input" },
+--     },
