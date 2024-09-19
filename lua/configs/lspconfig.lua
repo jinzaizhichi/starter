f32d53673431051a50e578bac7dab50698ffeeeb
jinzaizhichi <jinzaizhichi9888@gmail.com>
2024-09-19 16:15:12 +0800

    update

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -38,3 +38,14 @@ lspconfig.pyright.setup {
     },
   },
 }
+-- Configure `ruff-lsp`.
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+-- For the default config, along with instructions on how to customize the settings
+require("lspconfig").ruff_lsp.setup {
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    },
+  },
+}
