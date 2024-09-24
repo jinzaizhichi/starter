b4c1fb348a32c69ad36b80a2c8e0f94c71b88185
jinzaizhichi <jinzaizhichi9888@gmail.com>
2024-09-24 19:17:22 +0800

    update

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,51 +1,86 @@
--- EXAMPLE
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- -- EXAMPLE
+-- local on_attach = require("nvchad.configs.lspconfig").on_attach
+-- local on_init = require("nvchad.configs.lspconfig").on_init
+-- local capabilities = require("nvchad.configs.lspconfig").capabilities
+--
+-- local lspconfig = require "lspconfig"
+-- local servers = { "html", "cssls" }
+--
+-- -- lsps with default config
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = on_attach,
+--     on_init = on_init,
+--     capabilities = capabilities,
+--   }
+-- end
+--
+-- -- typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+-- }
+-- -- python
+-- lspconfig.pyright.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+--   filetype = { "python" },
+--   sigle_file_support = true,
+--   settings = {
+--     python = {
+--       analysis = {
+--         autoSearchPaths = true,
+--         diagnosticMode = "openFilesOnly",
+--         useLibraryCodeForTypes = true,
+--       },
+--     },
+--   },
+-- }
+-- -- Configure `ruff-lsp`.
+-- -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+-- -- For the default config, along with instructions on how to customize the settings
+-- require("lspconfig").ruff_lsp.setup {
+--   init_options = {
+--     settings = {
+--       -- Any extra CLI arguments for `ruff` go here.
+--       args = {},
+--     },
+--   },
+-- }
+local configs = require "nvchad.configs.lspconfig"
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = {
+  html = {},
+  awk_ls = {},
+  bashls = {},
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
+  ruff_lsp = {
+    init_options = {
+      settings = {
+        -- Any extra CLI arguments for `ruff` go here.
+        args = {},
+      },
+    },
+  },
 
--- typescript
-lspconfig.ts_ls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
--- python
-lspconfig.pyright.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-  filetype = { "python" },
-  sigle_file_support = true,
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        diagnosticMode = "openFilesOnly",
-        useLibraryCodeForTypes = true,
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          typeCheckingMode = "basic",
+        },
       },
     },
   },
 }
--- Configure `ruff-lsp`.
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
--- For the default config, along with instructions on how to customize the settings
-require("lspconfig").ruff_lsp.setup {
-  init_options = {
-    settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = {},
-    },
-  },
-}
+
+for name, opts in pairs(servers) do
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
+
+  require("lspconfig")[name].setup(opts)
+end
