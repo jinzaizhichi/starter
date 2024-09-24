28f19aa2135a08333ccd2247a218440196a9bd86
jinzaizhichi <jinzaizhichi9888@gmail.com>
2024-09-24 19:23:05 +0800

    added clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -6,4 +6,16 @@ local o = vim.o
 o.cursorlineopt = "both" -- to enable cursorline!
 -- 缺省行号为relativenumber
 -- o.ruler = true
-o.clipboard = "unnamedplus"
+-- o.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
