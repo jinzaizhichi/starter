8433eeb5c11770768f3cb2c331f52e313b58b7ba
jinzaizhichi <jinzaizhichi9888@gmail.com>
2024-09-27 17:19:17 +0800

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -78,7 +78,7 @@ return {
   },
   {
     "tpope/vim-surround",
-    version = "å*", -- Use for stability; omit to use `main` branch for the latest features
+    -- version = "å*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     -- config = function()
     --   require("nvim-surround").setup {}
