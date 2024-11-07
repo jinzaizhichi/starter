ade725e51185e17a27ffa8b25fc85bee68b2c529
jinzaizhichi <jinzaizhichi9888@gmail.com>
2024-11-07 16:04:50 +0800

    add  ruff

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -41,14 +41,14 @@ lspconfig.pyright.setup {
 -- Configure `ruff-lsp`.
 -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
 -- For the default config, along with instructions on how to customize the settings
--- require("lspconfig").ruff_lsp.setup {
---   init_options = {
---     settings = {
---       -- Any extra CLI arguments for `ruff` go here.
---       args = {},
---     },
---   },
--- }
+require("lspconfig").ruff.setup {
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    },
+  },
+}
 
 -- local configs = require "nvchad.configs.lspconfig"
 --
