7b878f2bb18820667134c7ee89d8e0fc8e75270d
jinzaizhichi <jinzaizhichi9888@gmail.com>
2025-02-26 16:46:53 +0800

    update

--- lua/options.lua
+++ lua/options.lua
@@ -1,41 +1,42 @@
-require "nvchad.options"
+require('nvchad.options')
 
 -- add yours here!
 local opt = vim.opt
 local o = vim.o
 opt.relativenumber = true
-o.cursorlineopt = "both" -- to enable cursorline!
+o.cursorlineopt = 'both' -- to enable cursorline!
 o.textwidth = 80
 o.wrap = true
+--o.formatoptions:append('t')
 -- 缺省行号为relativenumber
 -- o.ruler = true
 -- o.clipboard = "unnamedplus"
 vim.g.clipboard = {
-  name = "WslClipboard",
+  name = 'WslClipboard',
   copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
   },
   paste = {
-    ["+"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["*"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['+'] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
   },
   cache_enabled = 0,
 }
 
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd("BufReadPost", {
-  pattern = "*",
+autocmd('BufReadPost', {
+  pattern = '*',
   callback = function()
-    local line = vim.fn.line "'\""
+    local line = vim.fn.line('\'"')
     if
       line > 1
-      and line <= vim.fn.line "$"
-      and vim.bo.filetype ~= "commit"
-      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+      and line <= vim.fn.line('$')
+      and vim.bo.filetype ~= 'commit'
+      and vim.fn.index({ 'xxd', 'gitrebase' }, vim.bo.filetype) == -1
     then
-      vim.cmd 'normal! g`"'
+      vim.cmd('normal! g`"')
     end
   end,
 })
