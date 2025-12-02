db8cc3db183163f04723cd505213fd90c8b33bc6
jinzaizhichi <jinzaizhichi9888@gmail.com>
2025-12-02 17:40:29 +0800

    Modify conform.lua   2025-12-02 17:40

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,4 @@
 return {
-  {
-    'stevearc/conform.nvim',
-    event = 'BufWritePre', -- uncomment for format on save
-    opts = require('configs.conform'),
-  },
 
   -- These are some examples, uncomment them if you want to see them work!
 
@@ -25,7 +20,12 @@ return {
     -- },
     -- },
   },
-
+  {
+    'stevearc/conform.nvim',
+    event = 'BufWritePre', -- uncomment for format on save
+    opts = require('configs.conform'),
+  },
+  { 'zapling/mason-conform.nvim' },
   {
     'nvim-treesitter/nvim-treesitter',
     opts = {
