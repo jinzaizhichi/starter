7a9f88e56f607ba6f20f23869a80561daa6306e0
jinzaizhichi <jinzaizhichi9888@gmail.com>
2026-04-16 22:45:11 +0800

    移动Claude.nvim配置到init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,4 +25,31 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  { "christoomey/vim-tmux-navigator" },
+  {
+    "greggh/claude-code.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("claude-code").setup({
+        -- 终端窗口设置
+        window = {
+          position = "vertical", -- 竖向分屏，也可以改成 "horizontal" 或 "float"
+          split_ratio = 0.4,     -- 占屏幕宽度的 40%
+        },
+        -- 按键映射
+        keymaps = {
+          toggle = {
+            normal = "<leader>cc",  -- 普通模式切换
+            terminal = "<leader>cc", -- 终端模式切换
+          },
+          window_navigation = true,
+          scrolling = true,
+        },
+      })
+    end,
+  },
 }
